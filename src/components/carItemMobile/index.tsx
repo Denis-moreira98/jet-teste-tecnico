434fa17a9eb856662a3804600d46541d976b5984
Denis Moreira <72985107+Denis-moreira98@users.noreply.github.com>
2024-02-28 23:00:03 -0300

    feat: listando cars na table mobile

--- src/components/carItemMobile/index.tsx
+++ src/components/carItemMobile/index.tsx
@@ -1,44 +1,41 @@
-import ImgCar from "@/assets/car.png";
 import Image from "next/image";
 import { IoEllipsisVertical } from "react-icons/io5";
 import { RatingStar } from "../ratingStar";
 import { LabelStatusMobile } from "../labelStatus";
+import { CarProps } from "@/components/carItem/index";
 
-export function CarItemMobile() {
+export function CarItemMobile({ car }: { car: CarProps }) {
    return (
       <>
          <tr className="w-full border-b p-2 text-left flex flex-row items-center max-sm:justify-between">
             <div>
                <td className="pl-3">
                   <Image
-                     src={ImgCar}
-                     width={0}
-                     height={0}
-                     alt="carro"
-                     className="w-24 h-auto"
+                     src={car.image}
+                     width={90}
+                     height={65}
+                     alt={car.model}
                   />
                </td>
             </div>
-            <div className="flex flex-col ml-3">
-               <td className="font-semibold text-gray-900 mb-2">
-                  Mini Cooper 2020
-               </td>
+            <div className="flex flex-col">
+               <td className="font-semibold text-gray-900 mb-2">{car.model}</td>
                <td className="text-xs font-medium text-gray-500 mb-2">
-                  Next reservation: 12/12
+                  Next reservation: {car.date}
                </td>
 
                <td>
-                  <RatingStar rating={4} />
+                  <RatingStar rating={car.rating} />
                </td>
             </div>
             <td className="absolute">
-               <label className="relative left-3 bottom-8">
-                  <LabelStatusMobile status={true} />
+               <label className="relative left-3 bottom-6">
+                  <LabelStatusMobile status={car.status} />
                </label>
             </td>
             <div className="float-right">
                <td className="p-3">
-                  <button className="relative bottom-7">
+                  <button className="relative bottom-6">
                      <IoEllipsisVertical size={18} />
                   </button>
                </td>
